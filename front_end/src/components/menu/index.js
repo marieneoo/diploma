952fa8c2916675ec9dce4502225b92e58ecab277
marieneoo <149239439+marieneoo@users.noreply.github.com>
2024-04-05 12:03:32 -0700

    burger_success

--- front_end/src/components/menu/index.js
+++ front_end/src/components/menu/index.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./styles/menu.css"
-import Bar from "../menu/bar.js"
-import Main from "./main.js";
 import instagram from "../../assets/images/instagram.svg"
 import facebook from "../../assets/images/facebook.svg"
-import Burger from "./burger.js";
+import { NavLink } from "react-router-dom"
+import lotus from "../../assets/images/lotus_logo.svg"
+import burger from "../../assets/images/burger.svg"
+import Button from "../main_elements/button/button.js"
+
+
 export default function Menu() {
-    return (
-      <div className="menu">
-        <Bar/>
-        <Main/>
-        <Burger/>
-      </div>
-    );
+  const [barOpen, setBarOpen] = useState(false)
+
+  const barOpening = () => {
+    setBarOpen(!barOpen)
   }
+
+  return (
+    <div className="menu">
+      <div className='menu_container'>
+        <div className="bar">
+          <NavLink to="/home"><img className="lotus" src={lotus}></img></NavLink>
+          <ul>
+            <li><NavLink to="/home">ԳԼԽԱՎՈՐ</NavLink></li>
+            <li><NavLink to="/about">ՄԵՐ ՄԱՍԻՆ</NavLink></li>
+            <li><NavLink to="/classes">ԴԱՍԵՐ</NavLink></li>
+            <li><NavLink to="/contact">ԿԱՊ</NavLink></li>
+            <li><NavLink to="/spa">ՍՊԱ</NavLink></li>
+          </ul>
+
+        </div>
+        <div className="main">
+          <img className="instagram_img" src={instagram}></img>
+          <img className="facebook_img" src={facebook}></img>
+
+          <Button id="menu_button" txt="ԳՐԱՆՑՎԵԼ ՀԻՄԱ" path="/contact" img="null" />
+
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }} onClick={barOpening}>
+          <div className="burger">
+            <img className="burger_img" src={burger}></img>
+          </div>
+        </div>
+      </div>
+      {barOpen && <div className="burger_bar">
+        <ul>
+          <li><NavLink to="/home">HOME</NavLink></li>
+          <li><NavLink to="/about">ABOUT</NavLink></li>
+          <li><NavLink to="/classes">CLASSES</NavLink></li>
+          <li><NavLink to="/contact">CONTACT</NavLink></li>
+          <li><NavLink to="/spa">SPA</NavLink></li>
+        </ul>
+      </div>
+      }
+    </div>
+  );
+}
